f2a2a4353447b04dc5a7a776af63650ae9908594
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-10-13 21:17:41 +0300

    feat: add mapster

---
+++ FilmoSearch.BusinessLogicLayer/MappingConfigurations/GlobalMappingSettings.cs
@@ -0,0 +1,13 @@
+﻿using Mapster;
+
+namespace FilmoSearch.BusinessLogicLayer.MappingConfigurations
+{
+    public class GlobalMappingSettings : IRegister
+    {
+        public void Register(TypeAdapterConfig config)
+        {
+            TypeAdapterConfig.GlobalSettings.Default.MaxDepth(2);
+            TypeAdapterConfig.GlobalSettings.Default.PreserveReference(true);
+        }
+    }
+}
