0cb933d7817027dc074324cd485310c03ecf3498
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-10-13 21:15:10 +0300

    feat: add db context and di

---
+++ FilmoSearch.DataAcessLayer/Context/PortalContext.cs
@@ -0,0 +1,28 @@
+﻿using FilmoSearch.DataAcessLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace FilmoSearch.DataAcessLayer.Context
+{
+    public class PortalContext : DbContext
+    {
+        public PortalContext(DbContextOptions options) : base(options)
+        {
+            Database.Migrate();
+        }
+
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Film> Films { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            DataGenerator.Init();
+
+            modelBuilder.Entity<Film>().HasData(DataGenerator.Films);
+            modelBuilder.Entity<Review>().HasData(DataGenerator.Reviews);
+            modelBuilder.Entity<Actor>().HasData(DataGenerator.Actors);
+        }
+    }
+}
---
+++ FilmoSearch.DataAcessLayer/DI/ServicesConfguration.cs
@@ -0,0 +1,21 @@
+﻿using FilmoSearch.DataAcessLayer.Context;
+using FilmoSearch.DataAcessLayer.Repositories.Implementations;
+using FilmoSearch.DataAcessLayer.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace FilmoSearch.DataAcessLayer.DI
+{
+    public static class ServicesConfguration
+    {
+        public static void AddDataAccessDependencies(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<PortalContext>(options => options.UseNpgsql(configuration.GetConnectionString("DBConnection")));
+
+            services.AddTransient<IActorRepository, ActorRepository>();
+            services.AddTransient<IFilmRepository, FilmRepository>();
+            services.AddTransient<IReviewRepository, ReviewRepository>();
+        }
+    }
+}
