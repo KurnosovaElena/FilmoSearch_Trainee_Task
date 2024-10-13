234aa3e274562cac6e1972205bf7c55e5682c582
KurnosovaElena <138401728+KurnosovaElena@users.noreply.github.com>
2024-10-13 21:13:24 +0300

    feat: add entities

---
+++ FilmoSearch.DataAcessLayer/Entities/Film.cs
@@ -0,0 +1,12 @@
+﻿namespace FilmoSearch.DataAcessLayer.Entities
+{
+    public class Film
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public IEnumerable<Actor>? Actors { get; set; }
+        public IEnumerable<Review>? Reviews { get; set; }
+    }
+}
---
+++ FilmoSearch.DataAcessLayer/Entities/Review.cs
@@ -0,0 +1,14 @@
+﻿namespace FilmoSearch.DataAcessLayer.Entities
+{
+    public class Review
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public double Stars { get; set; }
+
+        public int FilmId { get; set; }
+        public Film Film { get; set; }
+    }
+}
